d504e073be43ad5b90ec48a488d63fe1027a6e4a
Angel Joven <72113587+Angel-Joven@users.noreply.github.com>
2021-10-01 12:20:02 -0500

    Creado con Visual Studio

---
+++ Tarea_02_EDD.cpp
@@ -0,0 +1,60 @@
+//Tarea_02_EDD.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//JOVEN JIMENEZ ANGEL CRISTIAN
+
+#include <iostream>
+
+int main() {
+	//Figura 01
+	printf("--------------------------------\nFigura 01\n");
+	printf("\n");
+	int n = 5;
+	for (int i = 1; i <= n; i = i + 1) {
+		for (int j = 1; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	//Figura 02
+	printf("--------------------------------\nFigura 02\n");
+	printf("\n");
+	int a = 5;
+	for (int i = 1; i <= a; i++) {
+		for (int j = 1; j <= a-i; j++) {
+			printf(" ");
+		}
+		for (int j = 1; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	//Figura 03
+	printf("--------------------------------\nFigura 03\n");
+	printf("\n");
+	int b = 5;
+	for (int i = b; i >= 1;i--) {
+		for (int j = 0; j < i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	//Figura 04
+	printf("--------------------------------\nFigura 04\n");
+	printf("\n");
+	int c = 5;
+	for (int i = c; i >= 1;i--) {
+		for (int j = 1; j <= c-i; j++) {
+			printf(" ");
+		}
+		for (int j = 1; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+/* Figura 01 |  Figura 02  |  Figura 03  |  Figura 04
+	*	     |      *	   |	*****	 |    *****
+	**       |     **	   |	****	 |	   ****
+	***		 |	  ***	   |	***		 |	    ***
+	****	 |	 ****	   |	**		 |	     **
+	*****	 |	*****	   |	*		 |	      *
+*/
